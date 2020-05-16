5d69bb222a7800c9e800b088e701b7e7cbcb0a6d
Abhishek <sanjay@dazeworks.com>
2020-05-16 16:39:02 +0530

    End of the day

--- force-app/main/default/aura/historicalWeatherComponent/historicalWeatherComponentController.js
+++ force-app/main/default/aura/historicalWeatherComponent/historicalWeatherComponentController.js
@@ -1,5 +1,61 @@
 ({
-    myAction : function(component, event, helper) {
+    /**
+     * Function to handle the initialization for the attribute
+     * @created by 16 May 2020
+     * @param {*} c 
+     * @param {*} e 
+     * @param {*} h 
+     */
+    doInit: function (c, e, h) {
+        try {
+            startTime();
+            function startTime() {
+                var today = new Date();
+                var h = today.getHours();
+                var m = today.getMinutes();
+                var s = today.getSeconds();
+                m = checkTime(m);
+                s = checkTime(s);
+                var currentTime = h + ":" + m + ":" + s;
+                c.set('v.currentTime', currentTime);
+                var t = setTimeout(startTime, 500);
+            }
+            function checkTime(i) {
+                if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+                return i;
+            }
+        } catch (e) {
+            console.error('Exception occurred while initalizing in do init. \n Message ::', e.message);
+        }
 
+    },
+
+    /**
+     * Function to handle the user input and validating the user input
+     * @created by 16 May 2020
+     * @param {*} c 
+     * @param {*} e 
+     * @param {*} h 
+     */
+    getWeatherHistory: function (c, e, h) {
+        try {
+            // Validating the input fields
+            var allValid = c.find('field').reduce(function (validSoFar, inputCmp) {
+                inputCmp.reportValidity();
+                return validSoFar && inputCmp.checkValidity();
+            }, true);
+            if (allValid) {
+                console.log('city ===>', c.get('v.city'));
+                console.log('startDate ===>', c.get('v.startDate'));
+                console.log('endDate ===>', c.get('v.endDate'));
+                // Calling helper method if all inputs are valid
+                h.getWeatherHistory_Helper(c,e,h);
+            } else {
+                console.error('Please update the invalid form entries and try again.');
+            }
+
+        } catch (e) {
+            console.error('Exception occurred while handling the button click. \n Message ::', e.message);
+        }
     }
 })
--- force-app/main/default/aura/historicalWeatherComponent/historicalWeatherComponentHelper.js
+++ force-app/main/default/aura/historicalWeatherComponent/historicalWeatherComponentHelper.js
@@ -1,5 +1,33 @@
 ({
-    helperMethod : function() {
+    /**
+     * helper method to handle the apex connect for get weather history button
+     * Created Date : 16 may 2020
+     * Created By : Abhishek Kumar Sharma
+     * @param {*} c 
+     * @param {*} e 
+     * @param {*} h 
+     */
+    getWeatherHistory_Helper: function (c, e, h) {
+        try {
 
+            let startDateInEpochFormat = new Date (c.get('v.startDate')).getTime()/1000.0;
+            let endDateInEpochFormat = new Date (c.get('v.endDate')).getTime()/1000.0;
+
+            let call_Apex = c.get('c.getHistoricalDataViaCityName_Apex');
+            call_Apex.setParams({
+                "city" : c.get('v.city'),
+                "startDate" : startDateInEpochFormat,
+                "endDate" : endDateInEpochFormat
+            });
+            call_Apex.setCallback(this, function(response){
+            console.log('state :::'+response.getState());
+            console.log('state :::'+JSON.stringify(response.getReturnValue()));
+            console.log('state :::'+response.getState());
+            });
+
+            $A.enqueueAction(call_Apex);
+        } catch (e) {
+                console.error('Exception :::::',e.message);
+        }
     }
 })
